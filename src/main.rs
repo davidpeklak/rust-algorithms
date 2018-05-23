d732e3c2c06706e9b5e02e47eb8b286d35c1231c
David Peklak <dpeklak@gmail.com>
2018-05-23 20:54:23 +0200

    percolation time measurement

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 extern crate algorithms;
 extern crate rand;
+extern crate stopwatch;
 
 use algorithms::percolation::{Percolation, PercolationState};
 use rand::prelude::*;
 use std::io;
+use stopwatch::Stopwatch;
 
 fn main() {
-    loop {
-        println!("Enter size:");
-        let size = read_i32();
-        if size < 0 {
-            break;
-        }
+    let mut size = 1;
+    let mut stopwatch = Stopwatch::new();
+    let mut elapsed_ms = 0i64;
+
+    while size <= 4096 {
+        stopwatch.reset();
+        stopwatch.start();
 
         let mut perc: PercolationState = Percolation::new(size as usize);
 
@@ -24,16 +27,27 @@ fn main() {
             perc.open(row, col);
         }
 
-        println!("{}", perc);
+        let new_elapsed_ms = stopwatch.elapsed_ms();
+
         let open_ratio = perc.number_of_open_sites() as f64 / (size * size) as f64;
-        println!("open ratio: {}", open_ratio);
+        println!("Size: {}, elapsed milliseconds: {}, open ratio: {}", size, new_elapsed_ms, open_ratio);
+
+        if elapsed_ms != 0 {
+            let ratio = new_elapsed_ms as f64 / elapsed_ms as f64;
+            println!("Ratio: {}", ratio);
+        }
+
+        elapsed_ms = new_elapsed_ms;
+
+        size = size * 2;
     }
 }
 
+/*
 fn read_i32() -> i32 {
     let mut s = String::new();
     io::stdin().read_line(&mut s).unwrap();
 
     s.trim_right().parse::<i32>().unwrap()
 }
-
+*/
