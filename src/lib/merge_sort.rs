9fec66cc1a8edf01aa492b8049f45cddb2c7b0d4
David Peklak <dpeklak@gmail.com>
2018-09-22 22:33:31 +0200

    implement merge-sort

--- src/lib/merge_sort.rs
+++ src/lib/merge_sort.rs
@@ -1,27 +1,60 @@
-/// Merges two parts of a vector. The first part of the vector is the range from the 0th element to
-/// the (mid-1)th element. The second part of the vector is the range from the mid-th element to the last
-/// element. If `mid > vec.len()`, the behaviour is undefined.
+pub fn merge_sort<Item>(vec: &mut Vec<Item>)
+    where Item: PartialOrd + Copy {
+    let mut aux_vec = vec.clone();
+    let size = vec.len();
+    do_merge_sort(vec, &mut aux_vec, 0, size);
+}
+
+/// recursivly merge-sort a vector, using an auxilary vector
+fn do_merge_sort<Item>(vec: &mut Vec<Item>, aux_vec: &mut Vec<Item>, lo: usize, hi: usize)
+    where Item: PartialOrd + Copy {
+    let mid = (lo + hi) / 2;
+
+    if mid - lo > 1 {
+        do_merge_sort(vec, aux_vec, lo, mid);
+    }
+
+    if hi - mid > 1 {
+        do_merge_sort(vec, aux_vec, mid, hi)
+    }
+
+    do_merge(vec, aux_vec, lo, hi, mid);
+}
+
+
+/// Merges two ranges of a vector. The first range is the range from the lo-th element to
+/// the (mid-1)th element. The second part of the vector is the range from the mid-th element to the
+/// (hi-1)th element.
 pub fn merge<Item>(vec: &mut Vec<Item>, mid: usize)
     where Item: PartialOrd + Copy {
+    let mut aux_vec = vec.clone();
     let size = vec.len();
-    let aux_vec = vec.clone();
-    let mut i = 0usize;
+    do_merge(vec, &mut aux_vec, 0, size, mid)
+}
+
+/// Merges two ranges of a vector, using an auxilary vector. The first range is the range from the lo-th element to
+/// the (mid-1)th element. The second part of the vector is the range from the mid-th element to the
+/// (hi-1)th element.
+fn do_merge<Item>(vec: &mut Vec<Item>, aux_vec: &mut Vec<Item>, lo: usize, hi: usize, mid: usize)
+    where Item: PartialOrd + Copy {
+    let mut i = lo;
     let mut j = mid;
 
-    for k in 0..size {
+    for k in lo..hi {
+        aux_vec[k] = vec[k];
+    }
+
+    for k in lo..hi {
         if i >= mid {
             vec[k] = aux_vec[j];
             j = j + 1;
-        }
-        else if j >= size {
+        } else if j >= hi {
             vec[k] = aux_vec[i];
             i = i + 1;
-        }
-        else if aux_vec[i] <= aux_vec[j] {
+        } else if aux_vec[i] <= aux_vec[j] {
             vec[k] = aux_vec[i];
             i = i + 1;
-        }
-        else {
+        } else {
             vec[k] = aux_vec[j];
             j = j + 1;
         }
@@ -30,12 +63,20 @@ pub fn merge<Item>(vec: &mut Vec<Item>, mid: usize)
 
 #[cfg(test)]
 mod test {
-use super::merge;
+    use super::{merge, merge_sort};
+    use is_sorted::is_sorted;
 
     #[test]
     fn merge_some() {
         let mut vec = vec![1, 4, 7, 1, 2, 3];
         merge(&mut vec, 3);
         assert_eq!(vec, vec![1, 1, 2, 3, 4, 7]);
+    }
+
+    #[test]
+    fn merge_sort_is_sorted() {
+        let mut vec = vec![7, 3, 5, 6, 2, 5, 1, 9];
+        merge_sort(&mut vec);
+        assert!(is_sorted(&vec));
     }
 }
